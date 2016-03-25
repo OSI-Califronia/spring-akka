cf55e6d1aebd0947d69b41c49d6488f3ca56520b
OSI-California <OSI-@gmx.net>
2016-03-25 21:15:30 +0100

    add initial project files

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by OSI on 25.03.2016.
- */
-public class Main {
-
-    public final static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+import java.util.logging.Level;
+
+import lombok.extern.java.Log;
+
+/**
+ * Created by OSI on 25.03.2016.
+ */
+@Log
+public class Main {
+
+	private void testerino() {
+		log.log(Level.INFO, "Starting the app");
+	}
+	
+    public final static void main(String[] args) {
+        
+    }
+}
